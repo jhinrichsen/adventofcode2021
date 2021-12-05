0f5347cbc69aca77046152c3133df7a8c4a8bfbc
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2021-12-05 08:40:51 +0100

    feat: add integer based implementation

--- day05.go
+++ day05.go
@@ -7,6 +7,77 @@ import (
 )
 
 func Day05(lines []string, part1 bool) (int, error) {
+	parse := func(line string) (Coordinate, error) {
+		var c Coordinate
+		var err error
+		parts := strings.Split(line, ",")
+		c.X, err = strconv.Atoi(parts[0])
+		if err != nil {
+			return c, err
+		}
+		c.Y, err = strconv.Atoi(parts[1])
+		return c, err
+	}
+
+	diagonal := func(c1, c2 Coordinate) bool {
+		return c1.X != c2.X && c1.Y != c2.Y
+	}
+
+	abs := func(i int) int {
+		if i < 0 {
+			return -i
+		}
+		return i
+	}
+
+	m := make(map[Coordinate]int)
+	for _, line := range lines {
+		parts := strings.Fields(line)
+		src, err := parse(parts[0])
+		if err != nil {
+			return 0, err
+		}
+		dst, err := parse(parts[2])
+		if err != nil {
+			return 0, err
+		}
+
+		// for part 1, only consider horizontal or vertical lines
+		if part1 && diagonal(src, dst) {
+			continue
+		}
+
+		dx := dst.X - src.X
+		if dx != 0 {
+			dx = dx / abs(dx)
+		}
+		dy := dst.Y - src.Y
+		if dy != 0 {
+			dy = dy / abs(dy)
+		}
+		inc := Coordinate{dx, dy}
+
+		c := src
+		for {
+			m[c]++
+			if c == dst {
+				break
+			}
+			c.X += inc.X
+			c.Y += inc.Y
+		}
+	}
+
+	var count int
+	for _, v := range m {
+		if v > 1 {
+			count++
+		}
+	}
+	return count, nil
+}
+
+func day05Cmplx(lines []string, part1 bool) (int, error) {
 	parse := func(line string) (complex128, error) {
 		parts := strings.Split(line, ",")
 		x, err := strconv.Atoi(parts[0])
