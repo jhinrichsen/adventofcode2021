fa1978bf7732d27ef7b0b619e72e67a2fde48e79
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2021-12-10 22:41:11 +0100

    feat: add day 10

---
+++ day10.go
@@ -0,0 +1,81 @@
+package adventofcode2021
+
+import (
+	"sort"
+	"strings"
+)
+
+func Day10(lines []string, part1 bool) uint {
+	pairs := []string{"[]", "{}", "()", "<>"}
+	points := []uint{57, 1197, 3, 25137}
+
+	// corrupted points or 0 if just incomplete
+	corrupted := func(s string) (total uint) {
+		for i := range s {
+			for j := range pairs {
+				if s[i] == pairs[j][1] {
+					return points[j]
+				}
+			}
+		}
+		return 0
+	}
+
+	var incompletes []string
+	var total uint
+	for _, line := range lines {
+		for {
+			changed := false
+			for j := 0; j < len(points); j++ {
+				before := len(line)
+				line = strings.Replace(line, pairs[j], "", 1)
+				if len(line) != before {
+					changed = true
+					break
+				}
+			}
+			if !changed {
+				break
+			}
+		}
+		// Check if incomplete or corrupted
+		n := corrupted(line)
+		if n == 0 {
+			incompletes = append(incompletes, line)
+		} else {
+			total += n
+		}
+	}
+	if part1 {
+		return total
+	}
+
+	// part 2
+	totals := make([]uint, len(incompletes))
+	for i, line := range incompletes {
+		total = 0
+		// We need to iterate from back to front when counting opening instead
+		// of closing pairs, the order is important because of constant
+		// multiplication factor
+		// No C style backward loop as in `for j := len(line); --j >=0; `
+		for j := len(line) - 1; j >= 0; j-- {
+			b := line[j]
+			total *= 5
+			if b == '(' {
+				total += 1
+			} else if b == '[' {
+				total += 2
+			} else if b == '{' {
+				total += 3
+			} else {
+				total += 4
+			}
+		}
+		totals[i] = total
+	}
+
+	// we have sort.IntSlice, but no sort.UIntSlice
+	sort.Slice(totals, func(i, j int) bool { return totals[i] < totals[j] })
+
+	return totals[len(totals)/2] // middle element
+}
---
+++ day10_test.go
@@ -0,0 +1,50 @@
+package adventofcode2021
+
+import "testing"
+
+func day10(t *testing.T, filename string, part1 bool, want uint) {
+	lines, err := linesFromFilename(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day10(lines, part1)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay10Part1Example(t *testing.T) {
+	day10(t, exampleFilename(10), true, 26397)
+}
+
+func TestDay10Part1(t *testing.T) {
+	day10(t, filename(10), true, 265527)
+}
+
+func TestDay10Part2Example(t *testing.T) {
+	day10(t, exampleFilename(10), false, 288957)
+}
+
+func TestDay10Part2(t *testing.T) {
+	day10(t, filename(10), false, 3969823589)
+}
+
+func bench10(b *testing.B, part1 bool) {
+	lines, err := linesFromFilename(filename(10))
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = Day10(lines, true)
+	}
+}
+
+func BenchmarkDay10Part1(b *testing.B) {
+	bench10(b, true)
+}
+
+func BenchmarkDay10Part2(b *testing.B) {
+	bench10(b, false)
+}
